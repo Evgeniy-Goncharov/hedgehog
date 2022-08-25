e6cc60f9f42518656808cd7350c14b73527fe5a7
siverskiy <siverskiy00@mail.ru>
2022-08-25 20:42:09 +0300

    Добавляет правки мобильной версии

--- source/js/modules/gallery.js
+++ source/js/modules/gallery.js
@@ -5,11 +5,18 @@ const galleryWrapper = document.querySelector('[data-slider=gallery]');
 export default function initSliders() {
   if (galleryWrapper) {
     const gallerySlider = new Swiper(galleryWrapper, {
-      slidesPerView: 2,
-      spaceBetween: 39,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      slidesPerView: 'auto',
+      spaceBetween: 34,
+
+      breakpoints: {
+        1280: {
+          slidesPerView: 2,
+          spaceBetween: 39,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+          },
+        },
       },
     });
 
